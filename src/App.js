c8b5c7e9dc9a54123e0ee62d533d7eca1c86437b
kjw-blog <rkdwjddnr11@naver.com>
2023-07-02 22:35:13 +0900

    Feat ) 최초의 커스텀 컴포넌트 생성

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import ExpenseItem from './components/ExpenseItem';
+
 function App() {
   return (
-    <div className='App'>
+    <div className="App">
       <h2>Let's get started!</h2>
-      <p>This is also visible!</p>
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
