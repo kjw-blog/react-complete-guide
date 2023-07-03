5a6266310fdf8bba9c70789c5279d596c998ccf4
kjw8319 <kjw8319@newconnect.co.kr>
2023-07-03 18:22:08 +0900

    Feat ) JSX에서 동적 데이터 출력 및 표현식 작업하기

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,17 @@
 import './ExpenseItem.css';
 
 function ExpenseItem() {
+  /** react 기능이 아닌 일반적인 js 문법이다. */
+  const expenseDate = new Date(2023, 6, 3);
+  const expenseTitle = 'Car Insurance';
+  const expenseAmount = 294.67;
+
   return (
-    <div className="expense-item">
-      <div>March 28th 2021</div>
-      <div className="expense-item__description">
-        <h2>Car Insurance</h2>
-        <div className="expense-item__price">$294.67</div>
+    <div className='expense-item'>
+      <div>{expenseDate.toISOString()}</div>
+      <div className='expense-item__description'>
+        <h2>{expenseTitle}</h2>
+        <div className='expense-item__price'>${expenseAmount}</div>
       </div>
     </div>
   );
