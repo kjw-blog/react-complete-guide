7f4c86d7780c6b9cf43305ffcad8b42468601ba1
kjw-blog <rkdwjddnr11@naver.com>
2023-07-31 00:13:13 +0900

    Feat ) 연습 프로젝트 폴더 이동 및 기본 프로젝트 세팅

--- src/App.js
+++ src/App.js
@@ -1,42 +1,5 @@
-import React, { useState } from 'react';
-import AddUser from './components/Users/AddUser';
-import UserList from './components/Users/UserList';
-import Warning from './components/Warning/Warning';
-
-function App() {
-  const [userList, setUserList] = useState([]);
-  const [warning, setWarning] = useState('');
-
-  const warningHandler = (text) => {
-    setWarning(text);
-  };
-  const warningResetHandler = () => {
-    setWarning('');
-  };
-
-  const deleteUserHandler = (id) => {
-    setUserList((prevUser) => prevUser.filter((user) => user.id !== id));
-  };
-
-  const addUserHandler = (user) => [
-    setUserList((prev) => {
-      const updateList = [...prev];
-      updateList.unshift(user);
-      return updateList;
-    }),
-  ];
-
-  return (
-    <>
-      <div>
-        <AddUser addUser={addUserHandler} onWarning={warningHandler} />
-        {userList.length > 0 && (
-          <UserList userList={userList} deleteUser={deleteUserHandler} />
-        )}
-      </div>
-      {warning && <Warning text={warning} close={warningResetHandler} />}
-    </>
-  );
-}
+const App = () => {
+  return <div></div>;
+};
 
 export default App;
