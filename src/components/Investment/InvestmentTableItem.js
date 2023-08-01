3511cfa019dedc2c2871db79378a52743f11e7cf
kjw-blog <rkdwjddnr11@naver.com>
2023-08-01 18:48:53 +0900

    Feat ) 연습하기2 진행중 Intl을 이용한 value 포맷

--- src/components/Investment/InvestmentTableItem.js
+++ src/components/Investment/InvestmentTableItem.js
@@ -1,15 +1,18 @@
 const InvestmentTableItem = ({ yearly }) => {
-  const valueHandler = (value) => {
-    return (+value.toFixed(2)).toLocaleString();
-  };
+  const formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  });
 
   return (
     <tr>
       <td>{yearly.year}</td>
-      <td>${valueHandler(yearly.savingsEndOfYear)}</td>
-      <td>${valueHandler(yearly.yearlyInterest)}</td>
-      <td>${valueHandler(yearly.totalInterest)}</td>
-      <td>${valueHandler(yearly.investedCapital)}</td>
+      <td>{formatter.format(yearly.savingsEndOfYear)}</td>
+      <td>{formatter.format(yearly.yearlyInterest)}</td>
+      <td>{formatter.format(yearly.totalInterest)}</td>
+      <td>{formatter.format(yearly.investedCapital)}</td>
     </tr>
   );
 };
