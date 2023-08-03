a15a4f0149836a90286835a350389a5f6bb77e9f
kjw-blog <rkdwjddnr11@naver.com>
2023-08-03 12:32:49 +0900

    Style ) 연습하기 강의  - 재사용 가능한 래퍼 컴포넌트 추가  - 사용자 입력 컴포넌트 스타일링

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,16 +1,21 @@
+import Card from '../UI/Card';
+import classes from './AddUser.module.css';
+
 const AddUser = () => {
   const addUserHandler = (e) => {
     e.preventDefault();
   };
 
   return (
-    <form onSubmit={addUserHandler}>
-      <label htmlFor="username">Username</label>
-      <input id="username" type="text" />
-      <label htmlFor="age">Age (Years)</label>
-      <input id="age" type="number" />
-      <button type="submit">Add User</button>
-    </form>
+    <Card className={classes.input}>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="username">Username</label>
+        <input id="username" type="text" />
+        <label htmlFor="age">Age (Years)</label>
+        <input id="age" type="number" />
+        <button type="submit">Add User</button>
+      </form>
+    </Card>
   );
 };
 
