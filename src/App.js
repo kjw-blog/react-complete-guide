b244ce9582179d8de32da7448ca82c5fa40940dc
kjw-blog <rkdwjddnr11@naver.com>
2023-08-07 23:16:37 +0900

    Feat ) useEffect 사용법

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
@@ -6,20 +6,37 @@ import MainHeader from './components/MainHeader/MainHeader';
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  console.log('test1');
 
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+
+    // 로그인 시 localStorage에 {isLoggedIn : "on"} 형식의 값을 저장한다
+    localStorage.setItem('isLoggedIn', 'on');
+
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
+    localStorage.removeItem('isLoggedIn');
     setIsLoggedIn(false);
   };
+
+  useEffect(() => {
+    const storedUserLoggedInInformation = localStorage.getItem('isLoggedIn');
+
+    console.log('test3');
+    if (storedUserLoggedInInformation === 'on') {
+      console.log('test4');
+      setIsLoggedIn(true);
+    }
+  }, []);
 
   return (
     <React.Fragment>
       <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+      {console.log('test2')}
       <main>
         {!isLoggedIn && <Login onLogin={loginHandler} />}
         {isLoggedIn && <Home onLogout={logoutHandler} />}
