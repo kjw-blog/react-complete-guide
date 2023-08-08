0b5374130177df4cbb0ac1cfeeb12b50e1a8dacb
kjw-blog <rkdwjddnr11@naver.com>
2023-08-08 17:40:04 +0900

    Memo ) useReducer 메모

--- src/App.js
+++ src/App.js
@@ -6,30 +6,23 @@ import MainHeader from './components/MainHeader/MainHeader';
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
-
     // 로그인 시 localStorage에 {isLoggedIn : "on"} 형식의 값을 저장한다
     localStorage.setItem('isLoggedIn', 'on');
-
     setIsLoggedIn(true);
   };
-
   const logoutHandler = () => {
     localStorage.removeItem('isLoggedIn');
     setIsLoggedIn(false);
   };
-
   useEffect(() => {
     const storedUserLoggedInInformation = localStorage.getItem('isLoggedIn');
-
     if (storedUserLoggedInInformation === 'on') {
       setIsLoggedIn(true);
     }
   }, []);
-
   return (
     <React.Fragment>
       <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
