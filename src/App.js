28dbcaa84aebf977376ad243c949286771a7e272
kjw-blog <rkdwjddnr11@naver.com>
2023-08-09 12:29:32 +0900

    Feat ) useReducer사용법 1. useReducer사용법 2. email reducer 생성

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useReducer, useState } from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
