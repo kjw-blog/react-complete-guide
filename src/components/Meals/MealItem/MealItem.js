ca3e7ac57b417066bc940729d857289c0f72e41b
kjw-blog <rkdwjddnr11@naver.com>
2023-08-14 23:10:12 +0900

    Feat ) 음식 수량 선택 Form 컴포넌트 추가  - 음식 수량 선택 Form 컴포넌트 추가  - 재사용 가능한 Input 컴포넌트 추가

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,4 +1,5 @@
 import Style from './MealItem.module.css';
+import MealItemForm from './MealItemForm';
 
 const MealItem = (props) => {
   const price = `$${props.price.toFixed(2)}`;
@@ -10,7 +11,9 @@ const MealItem = (props) => {
         <div className={Style.description}>{props.description}</div>
         <div className={Style.price}>{price}</div>
       </div>
-      <div></div>
+      <div>
+        <MealItemForm />
+      </div>
     </li>
   );
 };
--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,5 +1,23 @@
-const MealItemForm = () => {
-  return <></>;
+import Input from '../../UI/Input';
+import Style from './MealItemForm.module.css';
+
+const MealItemForm = (props) => {
+  return (
+    <form className={Style.form}>
+      <Input
+        label="Amount"
+        input={{
+          id: 'amount',
+          type: 'number',
+          min: '1',
+          max: '5',
+          step: '1',
+          defaultValue: '1',
+        }}
+      />
+      <button>+ Add</button>
+    </form>
+  );
 };
 
 export default MealItemForm;
