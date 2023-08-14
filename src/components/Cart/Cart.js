29277214aaf02a4b4fa04479c84a084a5a7aafa6
kjw-blog <rkdwjddnr11@naver.com>
2023-08-14 23:21:10 +0900

    Feat ) 장바구니 컴포넌트 추가  - 장바구니 컴포넌트 추가 및 퍼블리싱 적용  - 장바구니 더미데이터 추가

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,27 @@
+import Style from './Cart.module.css';
+
+const Cart = (props) => {
+  const cartItems = (
+    <ul className={Style['cart-items']}>
+      {[{ id: 'c1', name: 'Sushi', amount: 2, price: 12.99 }].map((item) => (
+        <li>{item.name}</li>
+      ))}
+    </ul>
+  );
+
+  return (
+    <div>
+      {cartItems}
+      <div className={Style.total}>
+        <span>Total Amount</span>
+        <span>35.62</span>
+      </div>
+      <div className={Style.actions}>
+        <button className={Style['button--alt']}>Close</button>
+        <button className={Style.button}>Order</button>
+      </div>
+    </div>
+  );
+};
+
+export default Cart;
