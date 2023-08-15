8cd5c1e4fe25b78833433ff60ccf80d196a92bac
kjw-blog <rkdwjddnr11@naver.com>
2023-08-15 22:39:49 +0900

    Feat ) 장바구니 Context에 담긴 상품 개수 계산

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,14 +1,22 @@
+import { useContext } from 'react';
 import CartIcon from '../Cart/CartIcon';
 import Style from './HeaderCartButton.module.css';
+import CartContext from '../../store/cart-context';
 
 const HeaderCartButton = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+    return curNumber + item.amount;
+  }, 0);
+
   return (
     <button className={Style.button} onClick={props.onClick}>
       <span className={Style.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={Style.badge}>3</span>
+      <span className={Style.badge}>{numberOfCartItems}</span>
     </button>
   );
 };
