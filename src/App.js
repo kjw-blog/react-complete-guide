01f9849a59105e3431ab2b949c918bd5c2d365aa
kjw-blog <rkdwjddnr11@naver.com>
2023-08-24 18:47:30 +0900

    Feat ) asnyc await로 fetch 사용

--- src/App.js
+++ src/App.js
@@ -6,22 +6,26 @@ import './App.css';
 function App() {
   const [movies, setMovies] = useState([]);
 
-  const fetchMoviesHandler = () => {
-    fetch('https://swapi.dev/api/films')
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        const transformedMovies = data.results.map((movieData) => {
-          return {
-            id: movieData.episode_id,
-            title: movieData.title,
-            releaseDate: movieData.release_date,
-            openingText: movieData.opening_crawl,
-          };
-        });
-        setMovies(transformedMovies);
-      });
+  const fetchMoviesHandler = async () => {
+    fetch('https://swapi.dev/api/films').then((res) => {
+      console.log(res);
+    });
+
+    const response = await fetch('https://swapi.dev/api/films');
+
+    console.log('response : ', response);
+
+    const data = await response.json();
+
+    const transformedMovies = data.results.map((movieData) => {
+      return {
+        id: movieData.episode_id,
+        title: movieData.title,
+        releaseDate: movieData.release_date,
+        openingText: movieData.opening_crawl,
+      };
+    });
+    setMovies(transformedMovies);
   };
 
   return (
