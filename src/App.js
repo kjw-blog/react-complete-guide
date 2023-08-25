8fdd2bf994b4f50d2b2fbfd453e907fd7f84c9f8
kjw-blog <rkdwjddnr11@naver.com>
2023-08-25 23:26:06 +0900

    Feat ) 데이터 페칭시 오류 해결방법

--- src/App.js
+++ src/App.js
@@ -6,34 +6,76 @@ import './App.css';
 function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   const fetchMoviesHandler = async () => {
     setIsLoading(true);
-    const response = await fetch('https://swapi.dev/api/films');
-    const data = await response.json();
+    setError(null);
 
-    const transformedMovies = data.results.map((movieData) => {
-      return {
-        id: movieData.episode_id,
-        title: movieData.title,
-        releaseDate: movieData.release_date,
-        openingText: movieData.opening_crawl,
-      };
-    });
-    setMovies(transformedMovies);
+    // fetch('https://swapi.dev/api/filmssss')
+    //   .then((res) => {
+    //     if (!res.ok) {
+    //       throw new Error('오류 발생');
+    //     }
+    //     return res.json();
+    //   })
+    //   .then((data) => {
+    //     const transformedMovies = data.results.map((movieData) => {
+    //       return {
+    //         id: movieData.episode_id,
+    //         title: movieData.title,
+    //         releaseDate: movieData.release_date,
+    //         openingText: movieData.opening_crawl,
+    //       };
+    //     });
+    //     setMovies(transformedMovies);
+    //   })
+    //   .catch((error) => console.log(error.message));
+    try {
+      const response = await fetch('https://swapi.dev/api/films');
+
+      if (!response.ok) {
+        throw new Error('오류 발생');
+      }
+
+      const data = await response.json();
+
+      const transformedMovies = data.results.map((movieData) => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          releaseDate: movieData.release_date,
+          openingText: movieData.opening_crawl,
+        };
+      });
+      setMovies(transformedMovies);
+    } catch (error) {
+      setError(error.message);
+    }
+
     setIsLoading(false);
   };
+
+  let content = <p>Found no movies</p>;
+
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />;
+  }
+
+  if (error) {
+    content = <p>{error}</p>;
+  }
+
+  if (isLoading) {
+    content = <p>Loading...</p>;
+  }
 
   return (
     <React.Fragment>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
-      <section>
-        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
-        {!isLoading && movies.length === 0 && <p>Found no movies.</p>}
-        {isLoading && <p>Loading...</p>}
-      </section>
+      <section>{content}</section>
     </React.Fragment>
   );
 }
