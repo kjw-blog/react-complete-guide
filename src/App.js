5f65aceab13208507391b2c59ef3ed50655f60f0
kjw-blog <rkdwjddnr11@naver.com>
2023-08-27 21:03:13 +0900

    Feat ) firebase 연결  - firebase 연결  - 영화 추가 api 설정 및 적용

--- src/App.js
+++ src/App.js
@@ -13,22 +13,27 @@ function App() {
     setIsLoading(true);
     setError(null);
     try {
-      const response = await fetch('https://swapi.dev/api/films');
+      const response = await fetch(
+        'https://react-http-f4305-default-rtdb.firebaseio.com/movies.json'
+      );
       if (!response.ok) {
         throw new Error('오류 발생');
       }
 
       const data = await response.json();
 
-      const transformedMovies = data.results.map((movieData) => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          releaseDate: movieData.release_date,
-          openingText: movieData.opening_crawl,
-        };
-      });
-      setMovies(transformedMovies);
+      const loadedMovies = [];
+
+      for (const key in data) {
+        // loadedMovies.push({ id: key, ...data[key] });
+        loadedMovies.push({
+          id: key,
+          title: data[key].title,
+          openingText: data[key].openingText,
+          releaseDate: data[key].releaseDate,
+        });
+      }
+      setMovies(loadedMovies);
     } catch (error) {
       setError(error.message);
     }
@@ -39,9 +44,26 @@ function App() {
     fetchMoviesHandler();
   }, [fetchMoviesHandler]);
 
-  function addMovieHandler(movie) {
-    console.log(movie);
-  }
+  const addMovieHandler = async (movie) => {
+    const response = await fetch(
+      'https://react-http-f4305-default-rtdb.firebaseio.com/movies.json',
+      {
+        method: 'POST',
+        body: JSON.stringify(movie),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+    );
+
+    const data = await response.json();
+
+    /**
+     * firebase에
+     * id: { ...movie } 형식으로 저장됨
+     * id는 firebase가 알아서 만들어줌
+     */
+  };
 
   let content = <p>Found no movies.</p>;
 
