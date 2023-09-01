e99d05a0b58f6e521f4fc0fd766d5d6f9c0eaafd
kjw-blog <rkdwjddnr11@naver.com>
2023-09-02 00:39:42 +0900

    Feat ) 리액트에서 Form 사용하는 기본적인 방법

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,9 +1,35 @@
+import { useState } from 'react';
+import { useRef } from 'react';
+
 const SimpleInput = (props) => {
+  const nameInputRef = useRef();
+  const [enteredName, setEnteredName] = useState('');
+
+  const nameInputChangeHandler = (e) => {
+    setEnteredName(e.target.value);
+  };
+
+  const formSubmissionHandler = (e) => {
+    e.preventDefault();
+    // 방법1. useState를 이용한 input value 설정
+    console.log(enteredName);
+
+    // 방법2. useRef를 이용한 input value 설정
+    const enteredValue = nameInputRef.current.value;
+    console.log(enteredValue);
+  };
+
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={formSubmissionHandler}>
+      <div className="form-control">
+        <label htmlFor="name">Your Name</label>
+        <input
+          ref={nameInputRef}
+          value={enteredName}
+          onChange={nameInputChangeHandler}
+          type="text"
+          id="name"
+        />
       </div>
       <div className="form-actions">
         <button>Submit</button>
