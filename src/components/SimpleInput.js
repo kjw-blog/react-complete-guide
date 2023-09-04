65cadb6dbc4a6a7b0194d8500661d3f19c46004b
kjw-blog <rkdwjddnr11@naver.com>
2023-09-04 23:33:13 +0900

    Feat) 과제 제출  - email input 추가  - '@'을 포함한 email 유효성 검증 추가

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -4,6 +4,9 @@ const SimpleInput = (props) => {
   const [enteredName, setEnteredName] = useState('');
   const [enteredNameTouched, setEnteredNameTouched] = useState(false);
 
+  const [enteredEmail, setEnteredEmail] = useState('');
+  const [enteredEmailTouched, setEnteredEmailTouched] = useState(false);
+
   /**
    * enteredNameIsValid의 값은 enteredName에 의해 정해지기 때문에 불필요한 상태값을 없애고
    * enteredName이 변경되면 해당 컴포넌트 재실행 후 enteredNameIsValid의 값이 다시 변경되기 때문에
@@ -12,9 +15,13 @@ const SimpleInput = (props) => {
   const enteredNameIsValid = enteredName.trim() !== '';
   const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
 
+  const enteredEmailIsValid =
+    enteredEmail.trim() !== '' && enteredEmail.includes('@');
+  const emailInputIsInvalid = !enteredEmailIsValid && enteredEmailTouched;
+
   let formIsValid = false;
 
-  if (enteredNameIsValid) {
+  if (enteredNameIsValid && enteredEmailIsValid) {
     formIsValid = true;
   }
 
@@ -25,20 +32,34 @@ const SimpleInput = (props) => {
   const nameInputBlurHandler = (e) => {
     setEnteredNameTouched(true);
   };
+  const emailInputChangeHandler = (e) => {
+    setEnteredEmail(e.target.value);
+  };
+
+  const emailInputBlurHandler = (e) => {
+    setEnteredEmailTouched(true);
+  };
 
   const formSubmissionHandler = (e) => {
     e.preventDefault();
     setEnteredNameTouched(true);
+    setEnteredEmailTouched(true);
 
-    if (!enteredNameIsValid) {
+    if (!enteredNameIsValid || !enteredEmailIsValid) {
       return;
     }
 
     setEnteredName('');
+    setEnteredEmail('');
     setEnteredNameTouched(false);
+    setEnteredEmailTouched(false);
   };
 
   const nameInputClasses = nameInputIsInvalid
+    ? 'form-control invalid'
+    : 'form-control';
+
+  const emailInputClasses = emailInputIsInvalid
     ? 'form-control invalid'
     : 'form-control';
 
@@ -55,6 +76,21 @@ const SimpleInput = (props) => {
         />
         {nameInputIsInvalid && (
           <p className="error-text">Name must not be empty.</p>
+        )}
+      </div>
+      <div className={emailInputClasses}>
+        <label htmlFor="email">Your E-Mail</label>
+        <input
+          value={enteredEmail}
+          onChange={emailInputChangeHandler}
+          onBlur={emailInputBlurHandler}
+          type="email"
+          id="email"
+        />
+        {emailInputIsInvalid && (
+          <p className="error-text">
+            Email must not be empty and must contain @
+          </p>
         )}
       </div>
       <div className="form-actions">
