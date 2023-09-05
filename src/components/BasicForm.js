ca7980c6f8a4c46dc2d91808ef716534f88ac76a
kjw-blog <rkdwjddnr11@naver.com>
2023-09-05 23:16:14 +0900

    Feat) 반복학습과제 진행 중  - firstName input 유효성 검증 추가

--- src/components/BasicForm.js
+++ src/components/BasicForm.js
@@ -1,10 +1,49 @@
+import { useState } from 'react';
+
 const BasicForm = (props) => {
+  const [firstName, setFirstName] = useState('');
+  const [firstNameTouched, setFirstNameTouched] = useState(false);
+
+  const firstNameIsValid = firstName.trim() !== '';
+  const firstNameHasError = !firstNameIsValid && firstNameTouched;
+
+  let formValid = false;
+  if (firstNameIsValid) formValid = true;
+
+  const firstNameChangeHandler = (e) => {
+    setFirstName(e.target.value);
+  };
+
+  const firstNameBlurHandler = (e) => {
+    setFirstNameTouched(true);
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    if (firstNameHasError) return;
+
+    setFirstName('');
+    setFirstNameTouched(false);
+  };
+
+  const firstNameClasses = `form-control ${firstNameHasError && 'invalid'}`;
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="control-group">
-        <div className="form-control">
+        <div className={firstNameClasses}>
           <label htmlFor="name">First Name</label>
-          <input type="text" id="name" />
+          <input
+            type="text"
+            id="name"
+            value={firstName}
+            onChange={firstNameChangeHandler}
+            onBlur={firstNameBlurHandler}
+          />
+          {firstNameHasError && (
+            <p className="error-text">이름을 입력해 주세요.</p>
+          )}
         </div>
         <div className="form-control">
           <label htmlFor="name">Last Name</label>
@@ -16,7 +55,7 @@ const BasicForm = (props) => {
         <input type="text" id="name" />
       </div>
       <div className="form-actions">
-        <button>Submit</button>
+        <button disabled={!formValid}>Submit</button>
       </div>
     </form>
   );
