d75c99672fd20842ce5d5728370b1e2d9b7dac26
kjw-blog <rkdwjddnr11@naver.com>
2023-09-06 23:04:36 +0900

    Feat) 과제 진행 중  - 데이터 패칭 에러 처리  - 없는 데이터에 대한 처리

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -5,23 +5,34 @@ import { useCallback, useEffect, useState } from 'react';
 
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
+  const [error, setError] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
 
   const getMealsData = useCallback(async () => {
+    setError(null);
     setIsLoading(true);
-    const response = await fetch(
-      'https://react-http-f4305-default-rtdb.firebaseio.com/meals.json'
-    );
+    try {
+      const response = await fetch(
+        'https://react-http-f4305-default-rtdb.firebaseio.com/meals.json'
+      );
 
-    const data = await response.json();
+      if (!response.ok) {
+        throw new Error('오류 발생');
+      }
 
-    let mealItems = [];
+      const data = await response.json();
 
-    for (let key in data) {
-      mealItems.push({ id: key, ...data[key] });
+      let mealItems = [];
+
+      for (let key in data) {
+        mealItems.push({ id: key, ...data[key] });
+      }
+
+      setMeals(mealItems);
+    } catch (e) {
+      setError(e.message);
+      console.log(e.message);
     }
-
-    setMeals(mealItems);
     setIsLoading(false);
   }, []);
 
@@ -29,21 +40,30 @@ const AvailableMeals = () => {
     getMealsData();
   }, [getMealsData]);
 
-  let mealsList = (
-    <ul>
-      {meals.map((meal) => (
-        <MealItem
-          key={meal.id}
-          id={meal.id}
-          name={meal.name}
-          description={meal.description}
-          price={meal.price}
-        />
-      ))}
-    </ul>
-  );
+  let mealsList = <p className={classes['text-center']}>데이터가 없습니다.</p>;
+
   if (isLoading) {
-    mealsList = <p className={classes.isLoading}>로딩중 입니다.</p>;
+    mealsList = <p className={classes['text-center']}>로딩중 입니다.</p>;
+  }
+
+  if (error) {
+    mealsList = <p className={classes['text-center']}>{error}</p>;
+  }
+
+  if (meals.length > 0) {
+    mealsList = (
+      <ul>
+        {meals.map((meal) => (
+          <MealItem
+            key={meal.id}
+            id={meal.id}
+            name={meal.name}
+            description={meal.description}
+            price={meal.price}
+          />
+        ))}
+      </ul>
+    );
   }
 
   return (
