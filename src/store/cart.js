a1f8cf45ea7c97a4ae020839924dbbc7130d3a45
kjw-blog <rkdwjddnr11@naver.com>
2023-09-18 23:26:27 +0900

    Feat) 복습 프로젝트 - store 구성 및 ui, cart slice 추가 - ui visible 상태에 따른 UI 변경 - addItemToCart Reducer함수 추가

---
+++ src/store/cart.js
@@ -0,0 +1,34 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  items: [],
+  totalQuantity: 0,
+};
+
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState,
+  reducers: {
+    addItemToCart(state, action) {
+      const newItem = action.payload;
+      const existingItem = state.item.find((item) => item.id === newItem.id);
+      if (!existingItem) {
+        state.items.push({
+          id: newItem.id,
+          price: newItem.price,
+          quantity: 1,
+          totalPrice: newItem.price,
+          name: newItem.title,
+        });
+      } else {
+        existingItem.quantity++;
+        existingItem.totalPrice = existingItem.totalPrice + newItem.price;
+      }
+    },
+    removeItemFromCart(state, action) {},
+  },
+});
+
+export const cartActions = cartSlice.actions;
+
+export default cartSlice.reducer;
