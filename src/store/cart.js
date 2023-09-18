a196afab85551e0e9c5a6481ddaaff489f0599b8
kjw-blog <rkdwjddnr11@naver.com>
2023-09-18 23:48:41 +0900

    Feat) 복습 프로젝트 - cart에 상품 추가, 삭제 로직 추가

--- src/store/cart.js
+++ src/store/cart.js
@@ -11,7 +11,8 @@ const cartSlice = createSlice({
   reducers: {
     addItemToCart(state, action) {
       const newItem = action.payload;
-      const existingItem = state.item.find((item) => item.id === newItem.id);
+      const existingItem = state.items.find((item) => item.id === newItem.id);
+      state.totalQuantity++;
       if (!existingItem) {
         state.items.push({
           id: newItem.id,
@@ -25,7 +26,17 @@ const cartSlice = createSlice({
         existingItem.totalPrice = existingItem.totalPrice + newItem.price;
       }
     },
-    removeItemFromCart(state, action) {},
+    removeItemFromCart(state, action) {
+      const id = action.payload;
+      const existingItem = state.items.find((item) => item.id === id);
+      state.totalQuantity--;
+      if (existingItem.quantity === 1) {
+        state.items = state.items.filter((item) => item.id !== id);
+      } else {
+        existingItem.quantity--;
+        existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
+      }
+    },
   },
 });
 
