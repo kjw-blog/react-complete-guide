addea71eaec06e43b8fb93ec4f72dfbf0652e440
kjw-blog <rkdwjddnr11@naver.com>
2023-09-19 23:10:13 +0900

    Feat) Redux Thunk를 이용한 firebase 데이터 가져오기  - Redux Thunk를 이용한 firebase 데이터 가져오기  - cart-slice에서 thunk 함수 분리

--- src/store/cart.js
+++ src/store/cart.js
@@ -1,5 +1,4 @@
 import { createSlice } from '@reduxjs/toolkit';
-import { uiActions } from './ui';
 
 const initialState = {
   items: [],
@@ -10,6 +9,10 @@ const cartSlice = createSlice({
   name: 'cart',
   initialState,
   reducers: {
+    replaceCart(state, action) {
+      state.items = action.payload.items;
+      state.totalQuantity = action.payload.totalQuantity;
+    },
     addItemToCart(state, action) {
       const newItem = action.payload;
       const existingItem = state.items.find((item) => item.id === newItem.id);
@@ -40,51 +43,6 @@ const cartSlice = createSlice({
     },
   },
 });
-
-export const sendCartData = (cartData) => {
-  return async (dispatch) => {
-    dispatch(
-      uiActions.showNotification({
-        status: 'pending',
-        title: 'Sending...',
-        message: ' Sending cart data!',
-      })
-    );
-    const sendRequest = async () => {
-      const response = await fetch(
-        'https://react-http-f4305-default-rtdb.firebaseio.com/cart.json',
-        {
-          method: 'PUT',
-          body: JSON.stringify(cartData),
-        }
-      );
-
-      if (!response.ok) {
-        throw new Error('Sending cart data failed.');
-      }
-    };
-
-    try {
-      await sendRequest();
-
-      dispatch(
-        uiActions.showNotification({
-          status: 'success',
-          title: 'Success!',
-          message: 'Sent cart data successfully !',
-        })
-      );
-    } catch (error) {
-      dispatch(
-        uiActions.showNotification({
-          status: 'error',
-          title: 'Error!',
-          message: 'Sending cart data failed!',
-        })
-      );
-    }
-  };
-};
 
 export const cartActions = cartSlice.actions;
 
