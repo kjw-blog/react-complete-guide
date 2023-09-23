488201e463c19bfd2383f37ffd6d9dd5db40019e
kjw-blog <rkdwjddnr11@naver.com>
2023-09-23 22:54:53 +0900

    Feat) 과제 진행 중 - 더미 이벤트 목록 이벤트 페이지에 추가 - 각 이벤트별 상세페이지 연결

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,5 +1,36 @@
+import EventsList from '../components/EventsList';
+
+const DUMMY_EVENTS = [
+  {
+    id: 'event_1',
+    image:
+      'https://blog.hubspot.de/hubfs/Germany/Blog_images/Optimize_Marketing%20Events%20DACH%202021.jpg',
+    title: '첫번째 이벤트',
+    date: '2023-09-22',
+  },
+  {
+    id: 'event_2',
+    image:
+      'https://blog.hubspot.de/hubfs/Germany/Blog_images/Optimize_Marketing%20Events%20DACH%202021.jpg',
+    title: '두번째 이벤트',
+    date: '2023-09-23',
+  },
+  {
+    id: 'event_3',
+    image:
+      'https://blog.hubspot.de/hubfs/Germany/Blog_images/Optimize_Marketing%20Events%20DACH%202021.jpg',
+    title: '세번째 이벤트',
+    date: '2023-09-24',
+  },
+];
+
 const EventsPage = () => {
-  return <h1>이벤트 목록 페이지</h1>;
+  return (
+    <main>
+      <h1>이벤트 목록 페이지</h1>
+      <EventsList events={DUMMY_EVENTS} />
+    </main>
+  );
 };
 
 export default EventsPage;
