328a21e9e29f011039b61c37b71b6f98605b377e
kjw-blog <rkdwjddnr11@naver.com>
2023-09-24 17:57:23 +0900

    Refactoring) 권장하는 loader 함수의 위치 적용

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,7 +1,21 @@
+import { useLoaderData } from 'react-router-dom';
 import EventsList from '../components/EventsList';
 
 function EventsPage() {
-  return <EventsList />;
+  const events = useLoaderData();
+
+  return <EventsList events={events} />;
 }
 
 export default EventsPage;
+
+export async function loader() {
+  const response = await fetch('http://localhost:8080/events');
+
+  if (!response.ok) {
+    // 오류 상황에 대한 처리
+  } else {
+    const resData = await response.json();
+    return resData.events;
+  }
+}
