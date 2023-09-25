4ea98a8e9a5a308f6d5bd831032af50e68a193fb
kjw-blog <rkdwjddnr11@naver.com>
2023-09-25 23:25:29 +0900

    Feat) loader에서 발생한 오류 처리 - 오류 데이터를 담고있는 Response 내보내기 - useRouteError 훅을 이용해서 해당 Response Data 가져오기

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -19,7 +19,9 @@ export async function loader() {
   const response = await fetch('http://localhost:8080/events');
 
   if (!response.ok) {
-    throw new Error('오류 발생');
+    throw new Response(JSON.stringify({ message: 'Could not fetch events.' }), {
+      status: 500,
+    });
   } else {
     return response;
   }
