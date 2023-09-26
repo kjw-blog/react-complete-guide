c57d92bc670a58dbc6b38f25d8f130ddc75d74dd
kjw-blog <rkdwjddnr11@naver.com>
2023-09-26 23:57:29 +0900

    Memo) useRouteLoaderData

--- frontend/src/pages/NewEvent.js
+++ frontend/src/pages/NewEvent.js
@@ -1,4 +1,10 @@
+import { useRouteLoaderData } from 'react-router-dom';
+
 const NewEventPage = () => {
+  const data = useRouteLoaderData('event-detail');
+
+  console.log('new에도 들어오니?', data);
+
   return <h1>NewEventPage</h1>;
 };
 
