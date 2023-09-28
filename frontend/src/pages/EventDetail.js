bf23fbfeaecd8522d6469dcfc41da8c03cbcd897
kjw-blog <rkdwjddnr11@naver.com>
2023-09-28 23:56:36 +0900

    Feat) defer로 여러개의 함수 제어하기

--- frontend/src/pages/EventDetail.js
+++ frontend/src/pages/EventDetail.js
@@ -1,17 +1,47 @@
-import { json, redirect, useRouteLoaderData } from 'react-router-dom';
+import {
+  Await,
+  defer,
+  json,
+  redirect,
+  useRouteLoaderData,
+} from 'react-router-dom';
 import EventItem from '../components/EventItem';
+import EventsList from '../components/EventsList';
+import { Suspense } from 'react';
 
 const EventDetailPage = () => {
-  const data = useRouteLoaderData('event-detail');
+  const { event, events } = useRouteLoaderData('event-detail');
 
-  return <EventItem event={data.event} />;
+  return (
+    <>
+      <Suspense fallback={<p style={{ textAlign: 'center' }}>Loading...</p>}>
+        <Await resolve={event}>
+          {(loadedEvent) => <EventItem event={loadedEvent} />}
+        </Await>
+      </Suspense>
+      <Suspense fallback={<p style={{ textAlign: 'center' }}>Loading...</p>}>
+        <Await resolve={events}>
+          {(loadedEvents) => <EventsList events={loadedEvents} />}
+        </Await>
+      </Suspense>
+    </>
+  );
 };
 
 export default EventDetailPage;
 
-export async function loader({ params }) {
-  const id = params.eventId;
+async function loadEvents() {
+  const response = await fetch('http://localhost:8080/events');
 
+  if (!response.ok) {
+    throw json({ message: 'Could not fetch events.' }, { status: 500 });
+  } else {
+    const resData = await response.json();
+
+    return resData.events;
+  }
+}
+async function loadEvent(id) {
   const response = await fetch('http://localhost:8080/events/' + id);
 
   if (!response.ok) {
@@ -20,8 +50,19 @@ export async function loader({ params }) {
       { status: 500 }
     );
   } else {
-    return response;
+    const resData = await response.json();
+
+    return resData.event;
   }
+}
+
+export async function loader({ params }) {
+  const id = params.eventId;
+
+  return defer({
+    event: await loadEvent(id),
+    events: loadEvents(),
+  });
 }
 
 export async function action({ request, params }) {
