9e820614ff42a9f2eb2e87a463bc952a466ea9e6
kjw-blog <rkdwjddnr11@naver.com>
2023-10-11 22:59:30 +0900

    Feat) 과제 - 이벤트 상세 정보 불러오기 및 화면에 적용

--- src/components/Events/EventDetails.jsx
+++ src/components/Events/EventDetails.jsx
@@ -1,8 +1,68 @@
-import { Link, Outlet } from 'react-router-dom';
+import { Link, Outlet, useParams } from 'react-router-dom';
 
 import Header from '../Header.jsx';
+import { useQuery } from '@tanstack/react-query';
+import { fetchEvent } from '../../util/http.js';
+import LoadingIndicator from '../UI/LoadingIndicator.jsx';
+import ErrorBlock from '../UI/ErrorBlock.jsx';
 
 export default function EventDetails() {
+  const { id } = useParams();
+
+  console.log(id);
+
+  const { data, isPending, isError, error } = useQuery({
+    queryKey: ['event', { eventId: id }],
+    queryFn: ({ signal }) => {
+      return fetchEvent({ id, signal });
+    },
+  });
+
+  let content = '아무것도 없음.';
+
+  if (isPending) {
+    content = <LoadingIndicator />;
+  }
+
+  if (isError) {
+    content = (
+      <ErrorBlock
+        title="오류 남."
+        message={error.info?.message || '오류 내용임'}
+      />
+    );
+  }
+
+  if (data) {
+    const { title, description, date, time, image, location } = data;
+
+    const src = 'http://localhost:3000/' + image;
+
+    content = (
+      <article id="event-details">
+        <header>
+          <h1>{title}</h1>
+          <nav>
+            <button>Delete</button>
+            <Link to="edit">Edit</Link>
+          </nav>
+        </header>
+        <div id="event-details-content">
+          <img src={src} alt="" />
+          <div id="event-details-info">
+            <div>
+              <p id="event-details-location">{location}</p>
+              <time dateTime={`Todo-DateT$Todo-Time`}>
+                {date} @ {time}
+              </time>
+            </div>
+            <p id="event-details-description">{description}</p>
+          </div>
+        </div>
+      </article>
+    );
+  }
+
   return (
     <>
       <Outlet />
@@ -11,25 +71,7 @@ export default function EventDetails() {
           View all Events
         </Link>
       </Header>
-      <article id="event-details">
-        <header>
-          <h1>EVENT TITLE</h1>
-          <nav>
-            <button>Delete</button>
-            <Link to="edit">Edit</Link>
-          </nav>
-        </header>
-        <div id="event-details-content">
-          <img src="http://localhost:3000/" alt="" />
-          <div id="event-details-info">
-            <div>
-              <p id="event-details-location">EVENT LOCATION</p>
-              <time dateTime={`Todo-DateT$Todo-Time`}>DATE @ TIME</time>
-            </div>
-            <p id="event-details-description">EVENT DESCRIPTION</p>
-          </div>
-        </div>
-      </article>
+      {content}
     </>
   );
 }
