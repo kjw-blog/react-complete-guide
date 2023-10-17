28c0f73a0141055716cea3538b09294d21388953
kjw-blog <rkdwjddnr11@naver.com>
2023-10-17 23:08:29 +0900

    Style) element가 사라질 때 애니메이션 실행

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -8,6 +8,7 @@ export default function Modal({ title, children, onClose }) {
       <motion.dialog
         initial={{ opacity: 0, translateY: 30 }}
         animate={{ opacity: 1, translateY: 0 }}
+        exit={{ opacity: 0, translateY: 30 }}
         open
         className="modal"
       >
