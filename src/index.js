7563264c5b1d521c012cf265e7ee71de01b44c6d
kjw-blog <rkdwjddnr11@naver.com>
2023-10-19 23:23:39 +0900

    Feat) 신규 프로젝트 세팅

--- src/index.js
+++ src/index.js
@@ -1,22 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { combineReducers, createStore } from 'redux';
-import { BrowserRouter, Router } from 'react-router-dom';
+import ReactDOM from 'react-dom/client';
 
 import './index.css';
 import App from './App';
-import productReducer from './store/reducers/products';
-
-const rootReducer = combineReducers({
-  shop: productReducer,
-});
-
-const store = createStore(rootReducer);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Router>
+  <React.StrictMode>
     <App />
-  </Router>
+  </React.StrictMode>
 );
