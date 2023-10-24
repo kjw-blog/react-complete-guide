22bc5c44bab85f75fab0c4d5f4b35cc4a96b531b
kjw-blog <rkdwjddnr11@naver.com>
2023-10-24 18:55:34 +0900

    Test & Feat) 테스트에 Act 단계 추가 - 테스트에 Act 단계 추가 - State 테스트 하기

--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import Greeting from './Greeting';
+import userEvent from '@testing-library/user-event';
 
 describe('Greeting component', () => {
   test('renders Hello World as a text', () => {
@@ -7,5 +8,37 @@ describe('Greeting component', () => {
 
     const helloWorldElement = screen.getByText('Hello World', { exact: false });
     expect(helloWorldElement).toBeInTheDocument();
+  });
+
+  test('renders good to see you if the button was NOT clicked', () => {
+    render(<Greeting />);
+
+    const outputElement = screen.getByText('good to see you', {
+      exact: false,
+    });
+
+    expect(outputElement).toBeInTheDocument();
+  });
+
+  test('renders "Changed!" if the button was clicked', () => {
+    render(<Greeting />);
+
+    const buttonElement = screen.getByRole('button');
+    userEvent.click(buttonElement);
+
+    const outputElement = screen.getByText('Changed!');
+    expect(outputElement).toBeInTheDocument();
+  });
+
+  test('does not render "good to see you" if you button was clicked', () => {
+    render(<Greeting />);
+
+    const buttonElement = screen.getByRole('button');
+    userEvent.click(buttonElement);
+
+    const outputElement = screen.queryByText('good to see you', {
+      exact: false,
+    });
+    expect(outputElement).toBeNull();
   });
 });
