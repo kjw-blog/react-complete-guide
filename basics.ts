c718af3ec733f900819ec9f65efb360d7f4ffdeb
kjw-blog <rkdwjddnr11@naver.com>
2023-10-25 23:03:28 +0900

    Feat) 타입스크립트 제네릭 사용

--- basics.ts
+++ basics.ts
@@ -58,6 +58,19 @@ function add(a: number, b: number): number {
   return a + b;
 }
 
-function print(value: any): void {
+function printOutput(value: any): void {
   console.log(value);
 }
+
+// 제네릭
+function insertAtBeginning<T>(array: T[], value: T) {
+  const newArray = [value, ...array];
+  return newArray;
+}
+
+const demoArray = [1, 2, 3];
+const updatedArray = insertAtBeginning(demoArray, -1);
+
+const stringArray = insertAtBeginning(['a', 'b', 'c'], 'd');
+
+stringArray[0].split('');
