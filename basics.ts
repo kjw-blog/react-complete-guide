5a1426ef9cb4319e72a306dfb96bbc2f57df1098
kjw-blog <rkdwjddnr11@naver.com>
2023-10-25 22:07:39 +0900

    Feat) 타입 지정 예시

---
+++ basics.ts
@@ -0,0 +1,47 @@
+// 기본형 타입
+let age: number = 24;
+
+age = 12;
+
+let userName: string;
+
+userName = 'Max';
+
+let isInstructor: boolean;
+
+isInstructor = true;
+
+// 복잡한 자료형 타입
+let hobbies: string[];
+
+hobbies = ['Sports', 'Cooking'];
+
+let person: {
+  name: string;
+  age: number;
+};
+
+person = {
+  name: 'Max',
+  age: 32,
+};
+
+let people: {
+  name: string;
+  age: number;
+}[];
+
+people = [
+  {
+    name: 'Max',
+    age: 32,
+  },
+  {
+    name: 'James',
+    age: 28,
+  },
+  {
+    name: 'John',
+    age: 51,
+  },
+];
