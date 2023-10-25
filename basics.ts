ae589a786524fd4bb8aaf2f49f34cef7ad7f851f
kjw-blog <rkdwjddnr11@naver.com>
2023-10-25 22:31:49 +0900

    Feat) Type Aliases 지정

--- basics.ts
+++ basics.ts
@@ -16,20 +16,19 @@ let hobbies: string[];
 
 hobbies = ['Sports', 'Cooking'];
 
-let person: {
+type Person = {
   name: string;
   age: number;
 };
+
+let person: Person;
 
 person = {
   name: 'Max',
   age: 32,
 };
 
-let people: {
-  name: string;
-  age: number;
-}[];
+let people: Person[];
 
 people = [
   {
